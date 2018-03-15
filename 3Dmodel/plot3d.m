616223ed558088cdc174de48e8740c1d25f0be53
Kevin Tang <kevin@calvisitor-10-105-164-22.calvisitor.1918.berkeley.edu>
2018-03-15 01:32:14 -0700

    2dplots

--- 3Dmodel/plot3d.m
+++ 3Dmodel/plot3d.m
@@ -1,4 +1,5 @@
 global folder;
+global folder2;
 folder = uigetdir();
 img = struct(dir(fullfile(folder,'*.jpg')));  
 A = []; % create the matrix
@@ -15,7 +16,20 @@ for k = 1: length(sortedNameNum) %iterate through the images
     % get the x,y coords of each red/orange pixel 
     B = red_pixel_coordinates(imageString);
     A = [A; B]; % vertically concat the two matrices 
-    D = blue_pixel_coordinates(imageString);
+end
+
+folder2 = uigetdir();
+img2 = struct(dir(fullfile(folder2,'*.jpg')));  
+nameNum2 = [];
+for k = 1: length(img2) % makes the names numerical 
+    nameNum2 = [nameNum2 [str2num(erase(img(k).name, '.jpg'))]];
+end;
+for k = 1: length(sortedNameNum) %iterate through the images
+    %'select' a particular image in the file 
+    image = sortedNameNum(k);
+    imageString = strcat(num2str(image), '.jpg'); 
+    % get the x,y coords of each red/orange pixel  
+    D = template_pixel_coordinates(imageString);
     C = [C; D];
 end
 drawPlot(A, C);
@@ -23,15 +37,15 @@ drawPlot(A, C);
 %get the red/orange pixel coordinates
 function B = red_pixel_coordinates(image)
     global folder;
-    sliceDepth = 0.000002; % subject to change
+    sliceDepth = 0.5; % subject to change
     % get the z-value
     imageNum = erase(image, '.jpg');
     nameNum = str2num(imageNum) * sliceDepth; % the z-value
     % set standard values for red/orange
-    rMin = 100;
+    rMin = 50;
     rMax = 255;
     gMin = 0;
-    gMax = 160;
+    gMax = 200;
     bMin = 0;
     bMax = 50;
     rgbImage = imread(fullfile(folder, image));
@@ -54,15 +68,19 @@ function B = red_pixel_coordinates(image)
     end
 end
 
-function D = blue_pixel_coordinates(image)
+function D = template_pixel_coordinates(image)
     global folder;
-    sliceDepth = 0.000002; % subject to change
+    sliceDepth = 0.5; % subject to change
     % get the z-value
     imageNum = erase(image, '.jpg');
     nameNum = str2num(imageNum) * sliceDepth; % the z-value
     % set standard values for red/orange
-    bMin = 190;
-    bMax = 255;
+    aMin = 100;
+    aMax = 110;
+    bMin = 100;
+    bMax = 110;
+    gMin = 100;
+    gMax = 110;
     rgbImage = imread(fullfile(folder, image));
     [height, width, ~] = size(rgbImage);
     D = zeros(height, 3);
@@ -80,17 +98,14 @@ function D = blue_pixel_coordinates(image)
 end
 
 function drawPlot(matrix1, matrix2) 
-    %C = repmat([1, 0.5 ,0],numel(matrix(:, 1), 1));
-    %c = C(:);
-    
     % remove all the zero rows
     TF1 = matrix1(:,1)==0; 
     matrix1(TF1, :) = [];
     TF2 = matrix2(:,1)==0;
     matrix2(TF2, :) = [];
-    s = scatter3(matrix2(:, 1), matrix2(:, 2), matrix2(:, 3), 'Filled');
+    s = scatter3(matrix2(:, 2), matrix2(:, 3), matrix2(:, 1), 'Filled');
     alpha(s, 0.05);
     hold on;
-    scatter3(matrix1(:, 1), matrix1(:, 2), matrix1(:, 3), 'Filled');
+    scatter3(matrix1(:, 2), matrix1(:, 3), matrix1(:, 1), 'Filled');
     rotate3d on
 end
